ca0edf114baa05d9b0a8547a3ecc4f0aa557bc3d
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-08-03 07:36:31 +0900

    Update Download and load 'data'.py

--- Download and load 'data'.py
+++ Download and load 'data'.py
@@ -12,6 +12,6 @@ print('dataset의 data 개수 :', len(dataset['train']))
 print('data의 정보 :', 'personality /', 'utterances = {candidates, history}')
 
 from transformers import GPT2Tokenizer, GPT2Model, GPT2Config
-config = GPT2Config.from_pretrained('gpt2', output_hidden_states=True)
-model = GPT2Model(config).from_pretrained('gpt2', config = config)
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+config = GPT2Config.from_pretrained("microsoft/DialoGPT-small" , output_hidden_states=True)
+model = GPT2Model(config).from_pretrained("microsoft/DialoGPT-small", config = config)
+tokenizer = GPT2Tokenizer.from_pretrained("microsoft/DialoGPT-small")

76fd1566969ed79b51abd6d799ee2251b634f276
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-08-02 19:47:22 +0900

    Update Download and load 'data'.py

--- Download and load 'data'.py
+++ Download and load 'data'.py
@@ -10,3 +10,8 @@ with open(dataset_File, mode = "r", encoding = "utf-8") as file:
 #0. Analyze 'dataset'
 print('dataset의 data 개수 :', len(dataset['train']))
 print('data의 정보 :', 'personality /', 'utterances = {candidates, history}')
+
+from transformers import GPT2Tokenizer, GPT2Model, GPT2Config
+config = GPT2Config.from_pretrained('gpt2', output_hidden_states=True)
+model = GPT2Model(config).from_pretrained('gpt2', config = config)
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
